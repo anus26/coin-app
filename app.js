8a7ba338318104bbe2270e37648eae26d0be153d
anas-raza~ <anusraza268@gmail.com>
2024-04-24 11:45:27 -0700

    index file created

---
+++ app.js
@@ -0,0 +1,13 @@
+ const h2=document.querySelector('h2')
+ function tossbtn(index){
+var x=Math.ceil(Math.random()*2)
+console.log(x)
+if(x === 1){
+    console.log('you won toss')
+    h2.innerHTML='you won toss'
+}else{
+    console.log('you loss toss');
+    h2.innerHTML='you loss toss'
+}
+     console.log(index)
+}
